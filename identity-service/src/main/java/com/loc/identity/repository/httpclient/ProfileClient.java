e83b5fc3cc997c0fa2e28659a2eca8b763ff3fa5
Lê Thành Lộc <106715293+thanhLoc1087@users.noreply.github.com>
2024-09-02 21:23:37 +0700

    add security among services

--- identity-service/src/main/java/com/loc/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/loc/identity/repository/httpclient/ProfileClient.java
@@ -5,10 +5,14 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.loc.identity.configuration.AuthenticationRequestInterceptor;
 import com.loc.identity.dto.request.ProfileCreationRequest;
 import com.loc.identity.dto.response.ProfileResponse;
 
-@FeignClient(name = "profile-service", url = "${application.service.profile}")
+@FeignClient(
+        name = "profile-service",
+        url = "${application.service.profile}",
+        configuration = {AuthenticationRequestInterceptor.class})
 public interface ProfileClient {
     @PostMapping(value = "/internal/users", produces = MediaType.APPLICATION_JSON_VALUE)
     ProfileResponse createProfile(@RequestBody ProfileCreationRequest request);
