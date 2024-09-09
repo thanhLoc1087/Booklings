20e6468d22d6a1e9058c3f0cd1bb3120a45405f0
Lê Thành Lộc <106715293+thanhLoc1087@users.noreply.github.com>
2024-09-09 10:12:41 +0700

    format post created time

--- post-service/src/main/java/com/loc/post/service/PostService.java
+++ post-service/src/main/java/com/loc/post/service/PostService.java
@@ -26,6 +26,7 @@ public class PostService {
 
     PostRepository postRepository;
     PostMapper postMapper;
+    DateTimeFormatter dateTimeFormatter;
 
     public PostResponse createPost(PostRequest request) {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -49,12 +50,19 @@ public class PostService {
         Pageable pageable = PageRequest.of(page - 1, size, sort);
 
         var pageData = postRepository.findAllByUserId(userId, pageable);
+
+        var postsList = pageData.getContent().stream().map(post -> {
+            var postResponse = postMapper.toPostResponse(post);
+            postResponse.setCreated(dateTimeFormatter.format(post.getCreatedDate()));
+            return postResponse;
+        }).toList();
+
         return PageResponse.<PostResponse>builder()
             .currentPage(page)
             .pageSize(pageData.getSize())
             .totalPages(pageData.getTotalPages())
             .totalElements(pageData.getTotalElements())
-            .data(pageData.getContent().stream().map(postMapper::toPostResponse).toList())
+            .data(postsList)
             .build();
     }
 }
